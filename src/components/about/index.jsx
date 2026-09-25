987224fd3788791cd2f7137f7f0dd57b93d547ee
Scooter <Scooter@example.com>
2026-09-24 23:56:09 -0700

    upload CV

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -36,7 +36,7 @@ const index = () => {
               {/* End personal info */}
 
               <div className="col-12 mt-1">
-                <a className="button" href="img/Manuel_Diaz_Resume.docx" download>
+                <a className="button" href="img/CV.pdf" download>
                   <span className="button-text">Download CV</span>
                   <span className="button-icon fa fa-download"></span>
                 </a>
